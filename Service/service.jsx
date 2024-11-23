acc81c4f6d8fb7ceaab4c08fe47c668e5ef1a817
parthjoshi2403200524032005 <parthjohi009@gmail.com>
2024-11-23 15:13:05 +0530

    url

--- Service/service.jsx
+++ Service/service.jsx
@@ -1,19 +1,18 @@
 import { Axiosinstance } from "./AxiosInterceptors";
 
 // export const url = "https://pj-tech-hub-backendmain.vercel.app";
-// export const url = "http://localhost:8080";
- export const url='https://pj-tech-hub-backend.vercel.app';
+//  export const url='http://localhost:8080';
+ export const url='https://pj-backend.vercel.app';
+
 
 export const userLogin = async (data) => {
-  return await Axiosinstance.post(`${url}/User/login`, data);
-};
-
-export const userSignup = async (data) => {
-  return await Axiosinstance.post(`${url}/User/register`, data);
-};
+    return await Axiosinstance.post(`${url}/User/login`, data);
+  };
+  
+  export const userSignup = async (data) => {
+    return await Axiosinstance.post(`${url}/User/register`, data);
+  };
 
 export const getsearch = async (searchQuery) => {
-  return await Axiosinstance.get(
-    `${url}/Role/searchRoles?searchTerm=${encodeURIComponent(searchQuery)}`
-  );
+  return await Axiosinstance.get(`${url}/Role/searchRoles?searchTerm=${searchQuery}`);
 };
